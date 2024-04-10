b281c1c60f63a6517fc57ce0e54a5d8d36deda3d
mbkamran <24100099@lums.edu.pk>
2024-04-11 00:19:58 +0500

    minor edits

--- server/routes/updatePassword.js
+++ server/routes/updatePassword.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { User } = require("../models/user");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const x = require("bcrypt");
 const findUserIdFromToken = require("../utils/findUserIdFromToken")
 
 // Route to update user password
