38750e4e282044e79a2ab091b516a37ca09cdce6
mbkamran <24100099@lums.edu.pk>
2024-04-13 16:01:47 +0500

    putting image in viewRestaurant

--- server/routes/viewRestaurant.js
+++ server/routes/viewRestaurant.js
@@ -2,9 +2,15 @@ const router = require("express").Router();
 const bcrypt = require("bcrypt");
 const { User } = require("../models/user");
 const { Restaurant, validateRestaurant } = require("../models/restaurant");
-const { FoodItem } = require("../models/fooditems")
+const { FoodItem } = require("../models/fooditems");
 const Joi = require("joi");
-const findUserIdFromToken = require("../utils/findUserIdFromToken")
+const findUserIdFromToken = require("../utils/findUserIdFromToken");
+const { ResOwner } = require("../models/resowner");
+const fs = require('fs');
+const path = require("path");
+
+
+const image_dir = path.join(__dirname, "../src/images/661710a74bdafdecfc45e96a.jpg");
 
 router.post("/", async (req, res) => {
 	try {
@@ -27,11 +33,25 @@ router.post("/", async (req, res) => {
         if (!restaurantTemp)
             return res.status(401).send({ message: "Could not find restaurant" });
 
-        const { warning_msg, __v, ...restaurant } = restaurantTemp.toObject();
+        // const { warning_msg, __v, ...restaurant } = restaurantTemp.toObject();
 
+        // res.status(200).send({ 
+        //     restaurant,
+        //     food_items 
+        // });
+
+        // const imagePath = "D:\LUMS Studies\Sem8\SE\Project_newnew\AI-enhanced-food-catalog\server\src\images\661710a74bdafdecfc45e96a.jpg"; // Assuming images are named with restaurant IDs
+        const image = fs.readFileSync(image_dir);
+        const base64Image = Buffer.from(image).toString('base64');
+    
+        const { warning_msg, __v, ...restaurantData } = restaurantTemp.toObject();
+    
         res.status(200).send({ 
-            restaurant,
-            food_items 
+          restaurant: {
+            ...restaurantData,
+            image: base64Image
+          },
+          food_items 
         });
 
 	} catch (error) {
